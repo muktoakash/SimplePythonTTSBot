1cad128246de751ce0501cf8c40eaa52701e2ef2
XcomReborn <zachary.coldrick@gmail.com>
2020-11-22 17:52:09 +0000

    First working commit

---
+++ SimpleTTSIRCBot.py
@@ -0,0 +1,122 @@
+import pyttsx3
+import socket
+import logging
+import sys
+import threading
+import queue
+from TTS import TTSThread
+
+for handler in logging.root.handlers[:]:
+	logging.root.removeHandler(handler)
+logging.basicConfig(format='%(asctime)s (%(threadName)-10s) [%(levelname)s] %(message)s', filename= 'errors.log',filemode = "w", level=logging.INFO)
+
+
+class IRCBot(threading.Thread):
+
+	def __init__(self):
+
+		threading.Thread.__init__(self)
+
+		self.server = 'irc.twitch.tv'
+		self.port = 6667
+
+		self.nick = 'COHopponentBot'
+		self.password = "oauth:6lwp9xs2oye948hx2hpv5hilldl68g"
+
+		self.channel = "#xcomreborn"
+
+		self.mytts = ttsSpeech()
+		self.mytts.start()
+
+		#create IRC socket
+		try:
+			self.irc = socket.socket()
+			self.irc.connect((self.server, self.port))
+		except Exception as e:
+			logging.error("A problem occurred creating a socket")
+			logging.error("In IRCBot")
+			logging.error(str(e))
+			self.irc.close()
+			sys.exit(0)
+
+		#sends variables for connection to twitch chat
+		self.irc.send(('PASS ' + self.password + '\r\n').encode("utf8"))
+		self.irc.send(('USER ' + self.nick + '\r\n').encode("utf8"))
+		self.irc.send(('NICK ' + self.nick + '\r\n').encode("utf8"))
+		self.irc.send(('CAP REQ :twitch.tv/membership' + '\r\n').encode("utf8")) # sends a twitch specific request necessary to recieve mode messages
+		self.irc.send(('CAP REQ :twitch.tv/tags'+ '\r\n').encode("utf8")) # sends a twitch specific request for extra data contained in the PRIVMSG changes the way it is parsed
+		self.irc.send(('CAP REQ :twitch.tv/commands' + '\r\n').encode("utf8")) # supposidly adds whispers
+
+		self.irc.send(('JOIN ' + self.channel + '\r\n').encode("utf8"))
+
+	def run(self):
+		self.running = True
+		self.irc.setblocking(0)	
+		readbuffer = ""
+		while self.running:
+			try:
+				readbuffer= readbuffer+self.irc.recv(1024).decode("utf-8")
+				temp=str.split(readbuffer, "\n")
+				readbuffer=temp.pop( )
+				for line in temp:
+					print(line)
+					value = line.split()
+					if (len(value) >= 5) and (value[2] == "PRIVMSG"):
+						userName = value[1]
+						userName = userName[1:]
+						userName = userName.split("!")[0]
+						message = " ".join(value [4:])
+						message = message[1:]
+						print("user : {0} , message {1}".format(userName, message))
+						self.CheckForUserCommand(userName, message)
+
+					if(value[0]=="PING"):
+							self.irc.send(("PONG %s\r\n" % line[0]).encode("utf8"))
+
+			except:
+				pass
+		self.irc.close()
+
+	def CheckForUserCommand(self, userName, message):
+		if (userName == "xcomreborn"):
+			if (message == "exit"):
+				self.mytts.queue.put(message)
+				self.running = False
+		voices = self.mytts.tts.engine.getProperty('voices')
+		if voices:
+			print("number of available voices {}".format(len(voices)))
+			mybytearray = bytearray(userName, 'utf-8')
+			myint = int.from_bytes(mybytearray, byteorder='big', signed=False)
+			myDefaultVoiceNumber = myint%len(voices)
+			print("user {} : default voice number {} ".format(userName, myDefaultVoiceNumber))
+			self.mytts.tts.engine.setProperty('voice',voices[myDefaultVoiceNumber].id)
+		self.mytts.queue.put(message)
+
+		
+		print("number of threads {0}".format(threading.active_count()))
+			
+
+
+class ttsSpeech(threading.Thread):
+
+	def __init__(self):
+		threading.Thread.__init__(self)
+		self.queue = queue.Queue()
+		self.tts = TTSThread()
+
+	def run(self):
+		while (True):
+			message = self.queue.get()
+			if message == "exit":
+				self.tts.terminate()
+				break
+			
+			self.tts.say(message)
+
+		print("exiting tts")
+	
+
+# The program propper starts here
+
+myNewBot = IRCBot()
+myNewBot.start()
---
+++ TTS.py
@@ -0,0 +1,82 @@
+# TTS.py
+import threading, time, pyttsx3
+
+
+class TTSThread(threading.Thread):
+    def __init__(self, rate=115, event=None):
+        super().__init__()
+
+        if event:
+            setattr(self, event, threading.Event())
+
+        self._cancel = threading.Event()
+        self.rate = rate
+        self.engine = None
+
+        self._say = threading.Event()
+        self._text_lock = threading.Lock()
+        self._text = []
+
+        self._is_alive = threading.Event()
+        self._is_alive.set()
+        self.start()
+
+    def _init_engine(self, rate):
+        engine = pyttsx3.init()
+        engine.setProperty('rate', rate)  # setting up new voice rate    
+        engine.connect('finished-utterance', self._on_completed)
+        engine.connect('started-word', self._on_cancel)
+        return engine
+
+    def say(self, text, stop=None):
+        if self._is_alive.is_set():
+            self._cancel.clear()
+
+            if isinstance(text, str):
+                text = [(text, stop)]
+
+            if isinstance(text, (list, tuple)):
+                for t in text:
+                    if isinstance(t, str):
+                        t = t, None
+
+                    with self._text_lock:
+                        self._text.append(t)
+
+                    self._say.set()
+
+    def cancel(self):
+        self._cancel.set()
+
+    def _on_cancel(self, name, location, length):
+        if self._cancel.is_set():
+            self.stop()
+
+    def stop(self):        
+        self.engine.stop()
+        time.sleep(0.5)
+        self.engine.endLoop()
+
+    def _on_completed(self, name, completed):
+        if completed:
+            self.engine.endLoop()
+            self.on_finished_utterance(name, completed)
+
+    def on_finished_utterance(self, name, completed):
+        pass
+
+    def terminate(self):
+        self._is_alive.clear()
+        self._cancel.set()
+        self.join()
+
+    def run(self):
+        self.engine = engine = self._init_engine(self.rate)
+        while self._is_alive.is_set():
+            while self._say.wait(0.1):
+                self._say.clear()
+
+                while not self._cancel.is_set() and len(self._text):
+                    with self._text_lock:
+                        engine.say(*self._text.pop(0))
+                    engine.startLoop()
